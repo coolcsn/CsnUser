45cf7e223ab7605783ba0bcff560a5477364371f
Stoyan Revov <st.revov@gmail.com>
2013-09-06 13:34:44 +0300

    First and Last name added to User entity.

--- src/CsnUser/Entity/User.php
+++ src/CsnUser/Entity/User.php
@@ -50,6 +50,28 @@ class User
      * @Annotation\Options({"label":"Display name:"})
      */
     protected $displayName;
+    
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="first_name", type="string", length=40, nullable=true)
+	 * @Annotation\Filter({"name":"StringTrim"})
+     * @Annotation\Validator({"name":"StringLength", "options":{"min":1, "max":40}})
+     * @Annotation\Attributes({"type":"text"})
+     * @Annotation\Options({"label":"Display name:"})
+     */
+    protected $firstName;
+    
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="last_name", type="string", length=40, nullable=true)
+	 * @Annotation\Filter({"name":"StringTrim"})
+     * @Annotation\Validator({"name":"StringLength", "options":{"min":1, "max":40}})
+     * @Annotation\Attributes({"type":"text"})
+     * @Annotation\Options({"label":"Display name:"})
+     */
+    protected $lastName;
 
     /**
      * @var string
@@ -258,6 +280,52 @@ class User
     public function getDisplayName()
     {
         return $this->displayName;
+    }
+    
+    /**
+     * Set firstName
+     *
+     * @param string $firstName
+     * @return User
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+    
+        return $this;
+    }
+
+    /**
+     * Get firstName
+     *
+     * @return string 
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+    
+    /**
+     * Set lastName
+     *
+     * @param string $lastName
+     * @return User
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+    
+        return $this;
+    }
+
+    /**
+     * Get lastName
+     *
+     * @return string 
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
     }
 
     /**
